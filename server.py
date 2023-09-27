238c3289eaa800183cb0c67190112463af32ecea
Altripayo <b.r.van.der.woude@student.rug.nl>
2023-09-27 11:21:44 +0200

    Agents change size with accumulated wealth

--- server.py
+++ server.py
@@ -6,13 +6,15 @@ from mesa.visualization.modules import CanvasGrid, ChartModule
 
 
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle", "Filled": "true", "r": 0.5}
+    portrayal = {"Shape": "circle", "Filled": "true"}
     portrayal["Color"] = "green"
     portrayal["Layer"] = 0
 
+    portrayal["r"] = max(0.1, min(1, (agent.money / 5000.0)) )
+
     if agent.trading:
         portrayal["Color"] = "red"
-        portrayal["Layer"] = 0        
+        portrayal["Layer"] = 0
 
     return portrayal
 
@@ -21,6 +23,6 @@ grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
 server = ModularServer(AgentModel, 
                        [grid], 
                        "Traders Model", 
-                       {"N":10, "width":10, "height":10})
+                       {"N":50, "width":10, "height":10})
 server.port = 8521  # The default
 server.launch()
